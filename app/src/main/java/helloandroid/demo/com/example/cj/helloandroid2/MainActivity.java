159be540c99ade7d820242620f032cffafcb38e0
CJSSSunshine <784303596@qq.com>
2018-01-24 15:25:05 +0800

    Initial commit

---
+++ app/src/main/java/helloandroid/demo/com/example/cj/helloandroid2/MainActivity.java
@@ -0,0 +1,14 @@
+package helloandroid.demo.com.example.cj.helloandroid2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO
+        setContentView(R.layout.activity_main);
+    }
+}
